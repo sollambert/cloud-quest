805b84a0106f0ae5aee7a7c1a4db7b4405334d6e
Solomon Lambert <sol.lambert@gmail.com>
2023-03-19 21:22:23 -0500

    allow user to keep multiple of same item

--- src/components/Pages/Inventory/Inventory.jsx
+++ src/components/Pages/Inventory/Inventory.jsx
@@ -16,18 +16,22 @@ function Inventory() {
                         : <>
                             <table>
                                 <tbody>
-                                    {items.map((item, i) => {
-                                        if (inventory.includes(item.id)) {
-                                            return (<tr key={i}>
-                                                <td>
-                                                    {item.name}
-                                                </td>
-                                                <td>
-                                                    {item.description}
-                                                </td>
-                                            </tr>)
-                                        }
-                                    })}
+                                    {inventory.map((id, i) => {
+                                        let item = items.filter((item, i) => {
+                                            if (item.id == id) {
+                                                return item;
+                                            }
+                                        })[0];
+                                        return (<tr key={i}>
+                                            <td>
+                                                {item.name}
+                                            </td>
+                                            <td>
+                                                {item.description}
+                                            </td>
+                                        </tr>)
+                                    })
+                                    }
                                 </tbody>
                             </table> </>
                     }

3f3de37106c6c57555c407b882267adb49115bdc
Solomon Lambert <sol.lambert@gmail.com>
2023-03-25 13:19:30 -0500

    add link for dummy deployment

--- src/redux/sagas/deploy.saga.js
+++ src/redux/sagas/deploy.saga.js
@@ -1,7 +1,10 @@
-import { takeLatest } from "redux-saga/effects"
+import { takeLatest, put } from "redux-saga/effects"
+import axios from "axios";
 
 function* deployCalculator() {
     yield console.log("DEPLOYING CALCULATOR YEEHAW");
+    let response = yield axios.get('/api/deploy');
+    yield put({type: "ADD_HISTORY", payload: `Application deployed to ${response.data.url}`});
 }
 
 function* deploySaga() {
